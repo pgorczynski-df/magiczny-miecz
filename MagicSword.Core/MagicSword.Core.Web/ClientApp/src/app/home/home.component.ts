ba2eecf0acf09e7e08308fe6aeb3bf8069190daf
Paweł Gorczyński <pawel.gorczynski@devfactory.com>
2018-07-04 12:53:11 +0200

    better sample

--- MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/home/home.component.ts
+++ MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-//import { Scene } from "@types/three";
-import THREE = require('three');
+import * as THREE from 'three';
+
+import orbit = require('three-orbit-controls');
+const OrbitControls = orbit(THREE);
 
 @Component({
   selector: 'app-home',
@@ -10,30 +12,259 @@ export class HomeComponent {
 
   constructor() {
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    /**
+ *
+ * WebGL With Three.js - Lesson 10 - Drag and Drop Objects
+ * http://www.script-tutorials.com/webgl-with-three-js-lesson-10/
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2015, Script Tutorials
+ * http://www.script-tutorials.com/
+ */
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    let sbVertexShader = [
+      "varying vec3 vWorldPosition;",
+      "void main() {",
+      "  vec4 worldPosition = modelMatrix * vec4( position, 1.0 );",
+      "  vWorldPosition = worldPosition.xyz;",
+      "  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+      "}",
+    ].join("\n");
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    let sbFragmentShader = [
+      "uniform vec3 topColor;",
+      "uniform vec3 bottomColor;",
+      "uniform float offset;",
+      "uniform float exponent;",
+      "varying vec3 vWorldPosition;",
+      "void main() {",
+      "  float h = normalize( vWorldPosition + offset ).y;",
+      "  gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( h, exponent ), 0.0 ) ), 1.0 );",
+      "}",
+    ].join("\n");
 
-    camera.position.z = 5;
+    let threeXWindowResize = function (renderer, camera) {
+      var callback = function () {
+        // notify the renderer of the size change
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // update the camera
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+      }
+      // bind the resize event
+      window.addEventListener('resize', callback, false);
+      // return .stop() the function to stop watching window resize
+      return {
+        /**
+         * Stop watching window resize
+        */
+        stop: function () {
+          window.removeEventListener('resize', callback);
+        }
+      };
+    }
 
-    var animate = () => {
-      requestAnimationFrame(animate);
+    var lesson10 = {
+      scene: null, camera: null, renderer: null,
+      container: null, controls: null,
+      clock: null, stats: null,
+      plane: null, selection: null, offset: new THREE.Vector3(), objects: [],
+      raycaster: new THREE.Raycaster(),
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      init: function () {
 
-      renderer.render(scene, camera);
+        // Create main scene
+        this.scene = new THREE.Scene();
+        this.scene.fog = new THREE.FogExp2(0xcce0ff, 0.0003);
+
+        var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+
+        // Prepare perspective camera
+        var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 1, FAR = 1000;
+        this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+        this.scene.add(this.camera);
+        this.camera.position.set(100, 0, 0);
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        // Prepare webgl renderer
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        this.renderer.setClearColor(this.scene.fog.color);
+
+        // Prepare container
+        this.container = document.createElement('div');
+        document.body.appendChild(this.container);
+        this.container.appendChild(this.renderer.domElement);
+
+        // Events
+        threeXWindowResize(this.renderer, this.camera);
+
+        document.addEventListener('mousedown', this.onDocumentMouseDown, false);
+        document.addEventListener('mousemove', this.onDocumentMouseMove, false);
+        document.addEventListener('mouseup', this.onDocumentMouseUp, false);
+
+        // Prepare Orbit controls
+        this.controls = new OrbitControls(this.camera);
+        this.controls.target = new THREE.Vector3(0, 0, 0);
+        this.controls.maxDistance = 150;
+
+        // Prepare clock
+        this.clock = new THREE.Clock();
+
+        // Prepare stats
+        //this.stats = new Stats();
+        //this.stats.domElement.style.position = 'absolute';
+        //this.stats.domElement.style.left = '50px';
+        //this.stats.domElement.style.bottom = '50px';
+        //this.stats.domElement.style.zIndex = 1;
+        //this.container.appendChild(this.stats.domElement);
+
+        // Add lights
+        this.scene.add(new THREE.AmbientLight(0x444444));
+
+        var dirLight = new THREE.DirectionalLight(0xffffff);
+        dirLight.position.set(200, 200, 1000).normalize();
+        this.camera.add(dirLight);
+        this.camera.add(dirLight.target);
+
+        // Display skybox
+        this.addSkybox();
+
+        // Plane, that helps to determinate an intersection position
+        this.plane = new THREE.Mesh(
+          new THREE.PlaneBufferGeometry(500, 500, 8, 8),
+          new THREE.MeshBasicMaterial({ color: 0xffffff, alphaTest: 0, visible: false }));
+        this.scene.add(this.plane);
+
+        // Add 100 random objects (spheres)
+        var object, material, radius;
+        var objGeometry = new THREE.SphereGeometry(1, 24, 24);
+        for (var i = 0; i < 50; i++) {
+          material = new THREE.MeshPhongMaterial({ color: Math.random() * 0xffffff });
+          material.transparent = true;
+          object = new THREE.Mesh(objGeometry.clone(), material);
+          this.objects.push(object);
+
+          radius = Math.random() * 4 + 2;
+          object.scale.x = radius;
+          object.scale.y = radius;
+          object.scale.z = radius;
+
+          object.position.x = Math.random() * 50 - 25;
+          object.position.y = Math.random() * 50 - 25;
+          object.position.z = Math.random() * 50 - 25;
+
+          this.scene.add(object);
+        }
+
+      },
+      addSkybox: function () {
+        var iSBrsize = 500;
+        var uniforms = {
+          topColor: { type: "c", value: new THREE.Color(0x0077ff) }, bottomColor: { type: "c", value: new THREE.Color(0xffffff) },
+          offset: { type: "f", value: iSBrsize }, exponent: { type: "f", value: 1.5 }
+        }
+
+        var skyGeo = new THREE.SphereGeometry(iSBrsize, 32, 32);
+        var skyMat = new THREE.ShaderMaterial({ vertexShader: sbVertexShader, fragmentShader: sbFragmentShader, uniforms: uniforms, side: THREE.DoubleSide, fog: false });
+        var skyMesh = new THREE.Mesh(skyGeo, skyMat);
+        this.scene.add(skyMesh);
+      },
+      onDocumentMouseDown: function (event) {
+        // Get mouse position
+        var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+        var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        // Get 3D vector from 3D mouse position using 'unproject' function
+        var vector = new THREE.Vector3(mouseX, mouseY, 1);
+        vector.unproject(lesson10.camera);
+
+        // Set the raycaster position
+        lesson10.raycaster.set(lesson10.camera.position, vector.sub(lesson10.camera.position).normalize());
+
+        // Find all intersected objects
+        var intersects = lesson10.raycaster.intersectObjects(lesson10.objects);
+
+        if (intersects.length > 0) {
+          // Disable the controls
+          lesson10.controls.enabled = false;
+
+          // Set the selection - first intersected object
+          lesson10.selection = intersects[0].object;
+
+          // Calculate the offset
+          var intersects2 = lesson10.raycaster.intersectObject(lesson10.plane);
+
+          lesson10.offset.copy(intersects2[0].point).sub(lesson10.plane.position);
+        }
+      },
+      onDocumentMouseMove: function (event) {
+        event.preventDefault();
+
+        // Get mouse position
+        var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+        var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        // Get 3D vector from 3D mouse position using 'unproject' function
+        var vector = new THREE.Vector3(mouseX, mouseY, 1);
+        vector.unproject(lesson10.camera);
+
+        // Set the raycaster position
+        lesson10.raycaster.set(lesson10.camera.position, vector.sub(lesson10.camera.position).normalize());
+
+        if (lesson10.selection) {
+          // Check the position where the plane is intersected
+          var intersects = lesson10.raycaster.intersectObject(lesson10.plane);
+          // Reposition the object based on the intersection point with the plane
+          lesson10.selection.position.copy(intersects[0].point.sub(lesson10.offset));
+        } else {
+          // Update position of the plane if need
+          var intersects2 = lesson10.raycaster.intersectObjects(lesson10.objects);
+          if (intersects2.length > 0) {
+            lesson10.plane.position.copy(intersects2[0].object.position);
+            lesson10.plane.lookAt(lesson10.camera.position);
+          }
+        }
+      },
+      onDocumentMouseUp: function (event) {
+        // Enable the controls
+        lesson10.controls.enabled = true;
+        lesson10.selection = null;
+      }
     };
 
-    animate();
+    // Animate the scene
+    function animate() {
+      requestAnimationFrame(animate);
+      render();
+      update();
+    }
+
+    // Update controls and stats
+    function update() {
+      var delta = lesson10.clock.getDelta();
+
+      lesson10.controls.update(delta);
+      //lesson10.stats.update();
+    }
+
+    // Render the scene
+    function render() {
+      if (lesson10.renderer) {
+        //console.log("rendering");
+        lesson10.renderer.render(lesson10.scene, lesson10.camera);
+      }
+    }
+
+    // Initialize lesson on page load
+    function initializeLesson() {
+      lesson10.init();
+      animate();
+    }
+
+    initializeLesson();
 
   }
 
