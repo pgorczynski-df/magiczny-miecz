d72d0ebfb8681a9e845ee2892bf2b59c04d8dbd5
Paweł Gorczyński <pawel.gorczynski@devfactory.com>
2018-07-13 10:51:24 +0200

    more playing around with dice

--- MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/game/Dice.ts
+++ MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/game/Dice.ts
@@ -6,6 +6,11 @@ export class Dice {
   object3D: THREE.Object3D;
   body: CANNON.Body;
 
+  previousResult = 0;
+  result = 0;
+
+  private throwing = false;
+
   constructor() {
 
     // Create a sphere
@@ -15,8 +20,9 @@ export class Dice {
       shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
     });
 
-    this.body.velocity.x = 20;
-    this.body.angularVelocity.z = -10;
+    
+    //this.body.velocity.x = 20;
+    //this.body.angularVelocity.z = -10;
   }
 
   init = (scene: THREE.Scene) => {
@@ -36,12 +42,56 @@ export class Dice {
       this.object3D.position.copy(<any>this.body.position);
       this.object3D.quaternion.copy(<any>this.body.quaternion);
     }
+
+    if (this.throwing) {
+      if (this.body.angularVelocity.almostZero(0.001)) {
+        this.result = this.readResult();
+        this.throwing = false;
+      }
+    }
   }
 
   throw = () => {
+
+    this.throwing = true;
+
+    this.previousResult = this.result;
+    this.result = 0;
+
     this.body.position.set(-10, 20, 0);
     this.body.velocity.x = 20;
     this.body.angularVelocity.z = -10;
+    this.body.angularVelocity.x = -10;
   }
 
+  private readResult = (): number =>  {
+    //var pi = Math.PI;
+    //var x = this.object3D.rotation.x;
+    //var y = this.object3D.rotation.y;
+    //var z = this.object3D.rotation.z;
+
+    //var delta = pi / 6;
+    
+
+    //if (Math.abs(x) < delta /*&& Math.abs(y) < delta*/ && Math.abs(z) < delta) {
+    //  return 2;
+    //}
+
+    //if (Math.abs(x - pi) < delta && Math.abs(y) < delta && Math.abs(z) < delta) {
+    //  return 4;
+    //}
+
+    //if (Math.abs(x) < delta && /*Math.abs(y) < delta &&*/ Math.abs(z - pi) < delta) {
+    //  return 6;
+    //}
+
+    //if (Math.abs(x - pi) < delta /*&& Math.abs(y) < delta*/ && Math.abs(z) < delta) {
+    //  return 2;
+    //}
+
+
+    return this.getRndInteger(1, 6);
+  }
+
+  getRndInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 }
