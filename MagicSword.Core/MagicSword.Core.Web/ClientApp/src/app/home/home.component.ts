7fcb34ddadb93a58af35f5831f85d4955ccb622d
Paweł Gorczyński <pawel.gorczynski@devfactory.com>
2018-07-23 13:41:14 +0200

    cleanup

--- MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/home/home.component.ts
+++ MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/home/home.component.ts
@@ -13,48 +13,13 @@ import {EventType} from "../game/EventType";
 })
 export class HomeComponent implements AfterViewInit {
 
-  //@ViewChild("viewport", { read: ElementRef }) viewport: ElementRef;
-
-  game: Game;
-  hub: GameHubClient;
-
-  get selectedActor(): IActor {
-    return this.game ? this.game.world.selectedActor : null;
-  }
-
   constructor(private services: Services) {
 
   }
 
   ngAfterViewInit() {
-    //this.hub = new GameHubClient(this.services);
-    //this.game = new Game(this.viewport.nativeElement, this.services);
+
   }
 
-  new = () => {
-    this.game.new();
-  }
-
-  save = () => {
-    this.game.save();
-  };
-
-  load = () => {
-    //this.game.load();
-
-    this.services.outboundBus.publish(EventType.GameLoadRequest);
-  };
-
-  drawCard = () => {
-    this.game.world.drawCard();
-  };
-
-  disposeCard = () => {
-    this.game.world.disposeCard();
-  };
-
-  sendMessage = () => {
-    this.hub.sendDirectMessage("dada", "userName");
-  }
 }
 
