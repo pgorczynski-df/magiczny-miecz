96883535f3c6788d52d607c880c2f706a7878315
Pawel Gorczynski <pawel.gorczynski@plgc.eu>
2018-08-09 13:38:03 +0200

    experimental nodejs project

---
+++ MagicSword.Core/MagicSword.Core.Logic/ClientApp/src/index.ts
@@ -0,0 +1,12 @@
+﻿import {App} from "./App"
+
+var app = new App().express;
+const port = /*process.env.PORT ||*/ 3000;
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    return console.log(`server is listening on ${port}`);
+});
